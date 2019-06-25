bfa082fd8f0f1b027eb9e1b1b02ee45ff83df085
Lauren <ljaimes92@gmail.com>
2019-06-25 15:02:43 -0400

    added array variables and a loop

---
+++ assets/javahava/game.js
@@ -0,0 +1,23 @@
+// themes words that are randomized 
+
+var cards = ["the hanged man", "the lovers", "the star", "strength", "the world", "the empress"];
+
+console.log(cards)
+
+// variables
+
+var guesses = 0
+var used = ""
+
+// randomize the words 
+
+for ( c = 0 ; c < cards.length; c++) {
+    console.log(cards[c]);
+    
+
+
+
+}
+
+
+
